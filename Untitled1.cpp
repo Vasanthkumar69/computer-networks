e010b1f35196a51937166a73197672ab5f2c9cd8
vasanth kumar <128572820+Vasanthkumar69@users.noreply.github.com>
2023-08-22 22:58:05 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ int i=0,count=0;
+ char databits[80];
+ printf("Enter Data Bits: ");
+ scanf("%s",databits);
+ printf("\nData Bits After Bit stuffing: ");
+  for(i=0; i<strlen(databits); i++)
+ {
+    if(databits[i]=='1')
+        count++;
+    else
+        count=0;
+    printf("%c",databits[i]);
+    if(count==5)
+    {
+        printf("0");
+        count=0;
+    }
+ }
+return 0 ;
+}
